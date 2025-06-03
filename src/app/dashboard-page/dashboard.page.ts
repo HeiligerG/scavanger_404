eaf4e31689599e3a3d184459aade6c4229719021
yannickict <yblatty@edp.ch>
2025-06-03 10:54:18 +0200

    Feat: - qr code - dashboard cleanup

--- src/app/dashboard-page/dashboard.page.ts
+++ src/app/dashboard-page/dashboard.page.ts
@@ -1,23 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
-import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: 'dashboard.page.html',
   styleUrls: ['dashboard.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class DashboardPage {
-  router = inject(Router);
-
-  goToGeolocation() {
-    (document.activeElement as HTMLElement)?.blur();
-    this.router.navigate(['/tabs/geolocation']);
-  }
-}
+export class DashboardPage {}
