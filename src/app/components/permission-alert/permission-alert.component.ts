44aaa3a1e845bd684e05e5cb7abf1762cbc4da5f
HeiligerG <gggigi111@gmail.com>
2025-06-03 13:53:53 +0200

    style: alert-component&add-permissioncomponent

---
+++ src/app/components/permission-alert/permission-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-permission-alert',
+  templateUrl: './permission-alert.component.html',
+  styleUrls: ['./permission-alert.component.scss'],
+})
+export class PermissionAlertComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
