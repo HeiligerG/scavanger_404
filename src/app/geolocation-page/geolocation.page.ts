3910607c39fd55d33e1050a8c339f4eda077713d
HolyG <gggigi111@gmail.com>
2025-06-03 21:27:11 +0200

    geolocation-Components_ts&html

--- src/app/geolocation-page/geolocation.page.ts
+++ src/app/geolocation-page/geolocation.page.ts
@@ -3,14 +3,17 @@ import { LocationService } from '../services/location.service';
 import { TimerService } from '../services/timer.service';
 import { Router } from '@angular/router';
 
+import { TaskCompleteAlertComponent } from '../components/task-complete-alert/task-complete-alert.component';
+import { FooterComponent } from '../components/footer/footer.component';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
+  IonText,
+  IonCard,
+  IonCardContent,
 } from '@ionic/angular/standalone';
-import { TaskCompleteAlertComponent } from '../components/task-complete-alert/task-complete-alert.component';
-import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-geolocation-page',
@@ -21,13 +24,22 @@ import { FooterComponent } from '../components/footer/footer.component';
     IonToolbar,
     IonTitle,
     IonContent,
+    IonText,
+    IonCard,
+    IonCardContent,
     TaskCompleteAlertComponent,
     FooterComponent,
   ],
 })
+
 export class GeolocationPage implements OnInit {
-  timerService = inject(TimerService);
-  router = inject(Router);
+  private location = inject(LocationService);
+
+  readonly distance = this.location.distance;
+  readonly target = this.location.targetPosition;
+
+  readonly timerService = inject(TimerService);
+  readonly router = inject(Router);
 
   completed = false;
   nextRoute = 'tabs/qr-code';
@@ -35,16 +47,19 @@ export class GeolocationPage implements OnInit {
   ngOnInit() {
     this.timerService.startTimer();
   }
+
   BackToDashboard() {
     this.timerService.resetTimer();
     this.BlurActiveElement();
 
     this.router.navigate(['tabs/dashboard']);
   }
+
   NextTask() {
     this.BlurActiveElement();
     this.router.navigate([this.nextRoute]);
   }
+
   SkipTask() {
     this.timerService.skipTimer('GeoLocation');
     this.BlurActiveElement();
